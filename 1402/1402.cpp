cc4447794c347b8bf688efcab8020074f7dd4244
denny.song <devforgood@gmail.com>
2022-03-25 00:19:12 +0900

    아무래도이문제는A번난이도인것같다

---
+++ 1402/1402.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        //int k = 2;
+        //vector<int> arr;
+        //while (a != 1)
+        //{
+        //    if (a % k == 0)
+        //    {
+        //        arr.push_back(k);
+        //        a /= k;
+        //    }
+        //    else
+        //    {
+        //        ++k;
+        //    }
+        //}
+
+        //int sum = 0;
+        //for (int i = 0; i < arr.size(); ++i)
+        //    sum += arr[i];
+
+        //if (sum == b)
+            cout << "yes" << "\n";
+        //else
+        //    cout << "no" << "\n";
+
+
+
+
+    }
+
+
+
+
+}
