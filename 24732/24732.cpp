a89c5231b7ff3b48f478dfcdfc87b5d46441b572
denny.song <denny.song@friendsgames.com>
2022-03-29 22:06:16 +0900

    잠자는 규리

--- 24732/24732.cpp
+++ 24732/24732.cpp
@@ -11,24 +11,13 @@ vector<long long> calc(const vector<long long>& v, int N) {
     vector<long long> ans(N);
     ans[0] = 1;
 
-    map<pair<int, int>, long long> dp;
-    for (int i = 0; i < N; ++i)
-        dp[{i, i}] = v[i];
-
-    for (int x = 1; x < N; x++) {
-        for (int y = 0; y < x; y++) {
-
-            dp[{x, y}] = min(dp[{x - 1, y}], v[x]);
-
-            //long long m = 1000000000000000000; // (1e18)
-            //for (int z = y; z <= x; z++) {
-            //    m = min(m, v[z]);
-            //}
-
-            ans[x] = ans[x] + ans[y] * dp[{x, y}];
-            ans[x] = ans[x] % 1000000007; // (1e9 + 7)
-        }
+    long long sum = 0;
+    for (int i = 1; i < N; ++i)
+    {
+        ans[i] = (sum + ans[i - 1] * min(v[i], v[i - 1])) % 1000000007;
+        sum = ans[i];
     }
+
 
     return ans;
 }
