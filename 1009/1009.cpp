2e2ca34d8f9631a4fbc729530a084c85d063acb3
denny.song <devforgood@gmail.com>
2022-04-12 22:42:24 +0900

    분산처리

---
+++ 1009/1009.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <map>
+#include <queue>
+#include <memory.h>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        int val = a % 10;
+        for (int i = 1; i < b; ++i)
+            val = val * a % 10 ;
+
+        val = val == 0 ? 10 : val;
+
+        cout << val << '\n';
+    }
+
+}
