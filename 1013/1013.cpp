005ba4b0ca544f188c4e728b99ad947334dc3311
denny.song <devforgood@gmail.com>
2022-04-13 23:38:03 +0900

    Contact

---
+++ 1013/1013.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <map>
+#include <queue>
+#include <memory.h>
+#include <regex>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    regex re("(100+1+|01)+");
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string str;
+        cin >> str;
+        if (regex_match(str, re))
+            cout << "YES" << '\n';
+        else
+            cout << "NO" << '\n';
+    }
+
+
+}
