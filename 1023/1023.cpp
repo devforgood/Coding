0942906b4029264f5f31c0601b0ba98a7eabc52f
DENNY_DESKTOP\devfo <devforgood@gmail.com>
2022-05-12 22:48:46 +0900

    괄호 문자열

---
+++ 1023/1023.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <map>
+#include <queue>
+#include <memory.h>
+#include <string>
+#include <deque>
+#include <iomanip>
+
+using namespace std;
+
+
+long long n, m;
+long long dp[51][2 * 51][2];
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="a">문자열 위치</param>
+/// <param name="b">열린 괄호 수</param>
+/// <param name="c">괄호 문자열 여부</param>
+/// <returns></returns>
+long long dfs(long long a, long long b, long long c) 
+{
+    if (a == n) 
+        return c || b;
+
+    if (dp[a][b + 51][c] != -1)
+        return dp[a][b + 51][c];
+
+    return dp[a][b + 51][c] = dfs(a + 1, b + 1, c) + dfs(a + 1, b - 1, c || b < 1);
+}
+
+void dfs2(long long a, long long b, long long c, long long k) 
+{
+    if (a == n) 
+        return;
+
+    if (dfs(a + 1, b + 1, c) <= k) 
+    {
+        cout << ')';
+        dfs2(a + 1, b - 1, c || b < 1, k - dfs(a + 1, b + 1, c));
+    }
+    else 
+    {
+        cout << '(';
+        dfs2(a + 1, b + 1, c, k);
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+    memset(dp, -1, sizeof(dp));
+    cin >> n >> m;
+    if (dfs(0, 0, 0) <= m) 
+        cout << -1;
+    else dfs2(0, 0, 0, m);
+
+}
